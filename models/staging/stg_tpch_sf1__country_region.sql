265c3f89ceee5346c386266058fdc4fe755b8a41
lorenzo.fioravanti@sdggroup.com <lorenzo.fioravanti@sdggroup.com>
2025-10-27 15:25:45 +0000

    added country_region and updated the src yaml with tests on FK

---
+++ models/staging/stg_tpch_sf1__country_region.sql
@@ -0,0 +1,14 @@
+WITH
+base_cr AS (
+    SELECT *
+    FROM {{ source("tpch_sf1", "region") }}
+),
+refined AS (
+    SELECT
+        r_regionkey AS region_id,
+        r_name AS name,
+        r_comment AS comment
+    FROM base_cr
+)
+SELECT *
+FROM refined
