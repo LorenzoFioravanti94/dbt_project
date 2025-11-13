1359b0a5f68ee872ba7a5a4960f9092f6005f55d
Lorenzo Fioravanti <lorenzo.fioravanti@sdggroup.com>
2025-11-13 14:15:33 +0000

    - added unique test on staging models 'customer' and 'supplier' on 'encrypted_address'. - corrected the schema in macro file 'grant_schema...'. - created snapshots for 'customer', 'part', 'supplier' sources.

--- macros/grant_schema_usage_and_select_permissions_to_public.sql
+++ macros/grant_schema_usage_and_select_permissions_to_public.sql
@@ -9,8 +9,8 @@
 {% macro grant_schema_usage_and_select_permissions_to_public(database, schemas) %}
     {# Loop over each schema provided in the list #}
     {% for schema in schemas %}
-        grant usage on schema {{ database }}.{{ schema | upper }} to role PUBLIC;
-        grant select on all tables in schema {{ database }}.{{ schema | upper }} to role PUBLIC;
-        grant select on all views in schema {{ database }}.{{ schema | upper }} to role PUBLIC;
+        grant usage on schema {{ database }}.LFIORAVANTI_{{ schema | upper }} to role PUBLIC;
+        grant select on all tables in schema {{ database }}.LFIORAVANTI_{{ schema | upper }} to role PUBLIC;
+        grant select on all views in schema {{ database }}.LFIORAVANTI_{{ schema | upper }} to role PUBLIC;
     {% endfor %}
 {% endmacro %}
